c0f47402d680a15085fe11a01e9072f07441c72b
sergeydomanov <nomad-labs@yandex.ru>
2022-08-17 20:48:47 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задание 1");
+        System.out.println();
 
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = (lastName + " " + firstName + " " + middleName);
+        System.out.println(fullName);
+
+        System.out.println("Задание 2");
+        System.out.println();
+
+        String upperCase = fullName.toUpperCase();
+        System.out.println(upperCase);
     }
 }
