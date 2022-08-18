6235cee845501639ebcc13f72a1cbf1c2a4b1838
sergeydomanov <nomad-labs@yandex.ru>
2022-08-18 21:11:03 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,24 @@ public class Main {
         System.out.println("Данные ФИО сотрудника — " + fullName2);
 
 
+        System.out.println("Задание 4");
+        System.out.println();
+
+        String fullName3 = "Ivanov Ivan Ivanovich";
+
+        String lastname3 = fullName3.substring (0, fullName3.indexOf(" "));
+        System.out.print("Фамилия сотрудника — " + lastname3);
+        System.out.println();
+        String firstname3 = fullName3.substring (7, 11);
+        System.out.print("Имя сотрудника — " + firstname3);
+        System.out.println();
+        String middleName3 = fullName3.substring (12, fullName3.lastIndexOf(""));
+        System.out.print("Отчество сотрудника — " + middleName3);
+
+//        StringBuilder lastName3 = new StringBuilder(fullName3)
+//                int = sb.substring(0, sb.indexOf(" "));
+
+
 
 
     }
